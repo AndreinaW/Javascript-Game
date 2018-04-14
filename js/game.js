2a000cd39028b5e801f522eb05b56fc889cce3eb
andreinaw <andreina_844@hotmail.com>
2018-04-14 20:19:52 +0200

    on a le 3 mechant

--- js/game.js
+++ js/game.js
@@ -7,6 +7,10 @@ let PLAYER_SPRITESHEET_URL = "images/player_skin.png";
 let canvas, ctx, player;
 let enemies = [];
 
+// spritesheet to load and loaded. When they are the same we can call requestAnimationFrame()
+let num_spritesheet = 1;       // one for the player, then we count the enemies
+let num_spritesheet_loaded = 0;
+
 
 //LEVEL NUMBER
 let lvl = 0;
@@ -22,32 +26,32 @@ function init() {
 
     environment = new Environment(BCKGRD__URL, CLOUDS_URL);
 
+
     $.getJSON('js/levels_settings.json').done(function (data) {
         data.levels[lvl].bricks.forEach(brick => {
             platforms.push(new Platform(brick.posX, brick.posY, brick.width, brick.height, brick.type, brick.src));
         });
+        
         data.levels[lvl].allEnemies.forEach(enemy => {
-            this_enemy = new Enemy(enemy.src, enemy.posX, enemy.posY);
-            console.log(enemy.src);
-            this_enemy.spritesheet.onload = function(){
-                this_enemy.initSprites();
-                enemies.push(this_enemy);
-            }
+            enemies.push(new Enemy(enemy.src, enemy.posX, enemy.posY));
+            num_spritesheet++;
         });
+
+        enemies.forEach((enemy) => {
+            enemy.spritesheet.onload = function(){
+                enemy.initSprites();   
+                num_spritesheet_loaded++;
+                checkRequestAnimation();
+            }
+        });                      
     });
+
     player = new Player(PLAYER_SPRITESHEET_URL, canvas.width/2, canvas.height/2);
     player.spritesheet.onload = function() {
-        player.initSprites();
-        requestAnimationFrame(animation);
+        player.initSprites();      
+        num_spritesheet_loaded++;
+        checkRequestAnimation();
     };
-/*
-    enemy = new Enemy(PLAYER_SPRITESHEET_URL, 0, 0);
-    enemy.spritesheet.onload = function() {
-        enemy.initSprites();
-        requestAnimationFrame(animation);
-    };
-    enemies.push(enemy);
-    */
 }
 
 
@@ -73,4 +77,11 @@ function moveAndDrawAllObjects() {
 
     player.move();
     player.draw(ctx);    
+}
+
+
+function checkRequestAnimation() {
+    if(num_spritesheet_loaded == num_spritesheet) {
+        requestAnimationFrame(animation);
+    }
 }
